73f9cfbdac65bdc40267520aa56440b489a5a67e
Raju Dhoni <32018260+rajusiripalli@users.noreply.github.com>
2025-06-05 22:17:43 +0530

    bottom tabs setup(0.36 mins)

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,33 @@
+import { Tabs } from 'expo-router';
+import { MaterialCommunityIcons, MaterialIcons, Foundation } from '@expo/vector-icons';
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <Foundation name="home" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name='newsAndHot'
+        options={{
+          title: 'New & Hot',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="play-box-multiple-outline" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name='myNetflix'
+        options={{
+          title: 'My Netflix',
+          tabBarIcon: ({ color }) => <MaterialIcons name="portrait" size={24} color={color} />,
+        }}
+      />
+    </Tabs>
+  )
+}
